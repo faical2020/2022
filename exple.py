97c52e24ebdc5c614e40e28142a73e2379a600ff
faical2020 <68774632+faical2020@users.noreply.github.com>
2024-02-22 18:35:18 +0100

    Update

---
+++ exple.py
@@ -0,0 +1,9 @@
+filename = r'C:\OCC.xlsx'
+
+append_df_to_excel(filename, df)
+
+append_df_to_excel(filename, df, header=None, index=False)
+
+append_df_to_excel(filename, df, sheet_name='Sheet2', index=False)
+
+append_df_to_excel(filename, df, sheet_name='Sheet2', index=False, startrow=25)
